dfccf935327daf943916875b25707bb59e3c32e3
Waldo Lavaut <waldothedeveloper@gmail.com>
2025-06-29 17:21:58 -0400

    shad cn ui added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import {
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex flex-col">
+      <Button>Click me</Button>
       <LoginLink>Sign in</LoginLink>
       <RegisterLink>Sign up</RegisterLink>
     </div>
