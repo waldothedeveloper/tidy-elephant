3d1ca3b6b852b48f227b77b2bd35e67a1f9d9878
Waldo Lavaut <waldothedeveloper@gmail.com>
2025-07-02 23:08:34 -0400

    kinde auth removed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,5 @@
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-
 export default function Dashboard() {
   return (
-    <div className="flex flex-col text-6xl font-bold text-black">
-      DASHBOARD
-      <LogoutLink>Log out</LogoutLink>
-    </div>
+    <div className="flex flex-col text-6xl font-bold text-black">DASHBOARD</div>
   );
 }
