1ec459d211ee8e757cf9cc4db692ce164645e5e3
Waldo Lavaut <waldothedeveloper@gmail.com>
2025-07-03 10:15:49 -0400

    clerk auth with waitlist complete

--- src/app/_custom_components/footer.tsx
+++ src/app/_custom_components/footer.tsx
@@ -1,8 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="bg-white">
+    <footer>
       <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <p className="mt-8 text-center text-sm/6 text-gray-600 md:order-1 md:mt-0">
+        <div className="flex justify-center gap-x-6 md:order-2">
+          <Button
+            variant="link"
+            className="text-muted-foreground hover:text-primary"
+          >
+            <Link href="/waitlist/join-us">Join the waitlist &rarr;</Link>
+          </Button>
+        </div>
+        <p className="text-center text-sm/6 text-muted-foreground md:order-1 md:mt-0">
           &copy; {new Date().getUTCFullYear()} Ease & Arrange, LLC.
         </p>
       </div>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,14 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
 export default function Dashboard() {
   return (
-    <div className="flex flex-col text-6xl font-bold text-black">DASHBOARD</div>
+    <div className="flex flex-col text-6xl font-bold text-foreground text-center items-center justify-center min-h-screen">
+      DASHBOARD
+      <div className="mt-8">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
   );
 }
