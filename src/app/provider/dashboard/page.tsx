9d2e80c69510884334b04c106003701f847b3158
Waldo Lavaut <waldothedeveloper@gmail.com>
2025-07-13 18:36:26 -0400

    Starting to build the provider onboarding flow

--- src/app/provider/dashboard/page.tsx
+++ src/app/provider/dashboard/page.tsx
@@ -13,7 +13,7 @@ export default function Dashboard() {
   useEffect(() => {
     const signInToFirebaseWithClerk = async () => {
       /* ***
-      CLERK AUTH:
+          CLERK AUTH:
           This method uses a cache so a network request will only be made if the token in memory has expired. The TTL for a Clerk token is one minute.      
           Tokens can only be generated if the user is signed in.
       *** */
@@ -26,7 +26,7 @@ export default function Dashboard() {
           // TODO: we could use js-cookie or similar later on for more robust cookie handling: https://www.npmjs.com/package/js-cookie
           document.cookie = `__firebase__session=${fireIDToken}; path=/dashboard; secure; SameSite=Strict`;
         } else {
-          console.error("Failed to sign in to Firebase");
+          // console.error("Failed to sign in to Firebase");
           document.cookie = `__firebase__session=; path=/dashboard; secure; SameSite=Strict`;
         }
       } catch (error) {
@@ -41,12 +41,12 @@ export default function Dashboard() {
   }, [getToken, userId]);
 
   if (authError) {
-    return <div>Something went wrong trying to sign in</div>;
+    throw new Error(`Failed to sign in to Firebase: ${authError}`);
   }
 
   return (
     <div className="flex flex-col text-6xl font-bold text-foreground text-center items-center justify-center min-h-screen">
-      DASHBOARD
+      PROVIDER DASHBOARD
       <div className="mt-8">
         <SignedIn>
           <UserButton />
